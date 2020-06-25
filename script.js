2a1e42c6c9378d782a57e58d9f724434528ba4f8
Sitaraman S <sitaraman.raghav.india@gmail.com>
2020-06-25 12:36:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//main-file js code
+$(document).ready(function () {
+    $("#a-open").click(function () {
+        $("#removeclass").removeClass("open");
+        $("#navbar").removeClass("not-clicked");
+        $("#navbar").addClass("clicked");
+        $("#addclass").addClass("close");
+        $("#dropdown").removeClass("dropdown");
+    });
+
+    $("#a-close").click(function () {
+        $("#dropdown").addClass("dropdown");
+        $("#removeclass").addClass("open");
+        $("#addclass").removeClass("close");
+        $("#navbar").addClass("not-clicked");
+        $("#navbar").removeClass("clicked");
+    });
+});
+
+
